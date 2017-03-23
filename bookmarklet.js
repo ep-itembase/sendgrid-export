674b998066a0870321dcfaa5f5bf0471a67c7960
ep-itembase <ep@itembase.biz>
2017-03-23 16:38:33 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -33,7 +33,7 @@ var result = "";
     valid = (typeof email !== 'null');
     
     var category = $(this).find("td.category").text();
-    var linkURL = $(this).find("td.div.div.div.dl.dd.url").text();
+    var linkURL = $(this).find("td.div.div.div.url").text();
     var date = $(this).find("td.time").text();
     var type = $(this).find("td.type").find("span.name").text();
     if(valid){
